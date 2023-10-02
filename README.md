09b95dbc8fe72414065d3fa2d47de5a3baf63440
SoyDaviti <dschaguaro@hotmail.com>
2023-10-01 20:03:41 -0500

    Proyecto Final

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # pruebafinal.com
 Proyecto final de Tratamiento de datos con Selenium - Autos usados Patiotuerca
+
+#Descripción
+Se define extraer datos de una fuente de datos online, en este caso se realiza
+la extracción de datos de una empresa de Ecuador que se dedica a la venta
+de automóviles de segunda mano, con la finalidad de realizar el tratamiento de datos,
+como los modelos de autos, sus precios, su año de fabricación. La información
+extraída se almacena en una base de datos MongoDB. Como segunda parte de la prueba
+se crea un servicio web para manejar esta data extraída.
+Se escoge la página online de la empresa Ecuador - Patiotuercas
+
+#PASO 1.
+Crear un archivo 'requirements.txt'
+
+Instalar las librerías:
+´´´
--- main.py
+++ main.py
@@ -2,18 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
+from mongo import MongoConnection
+
+db_client = MongoConnection().client
+db = db_client.get_database('Pruebafinal')
+col = db.get_collection('autos')
+
 driver = webdriver.Chrome()
 driver.get("https://ecuador.patiotuerca.com/usados/-/autos?type_autos_moderated=moderated")
-autos = driver.find_elements(By.CLASS_NAME, "vehicle-container")
-#for a in autos:
-   #auto_name = a.find_element(by=By.TAG_NAME, value="img").accessible_name
-   #auto_modelo = a.find_element(by=By.CLASS_NAME, value="module tittle").text
-   #auto_detalle = a.find_element(by=By.CLASS_NAME, value="latam-secondary-text test-lighten-2 left vehicle-highlight").text
-   #auto_precio = a.find_element(by=By.CLASS_NAME, value="price-text").text
-   #print(auto_name)
-   #print(auto_modelo)
-   #print(auto_detalle)
-   #print(auto_precio)
+autos = driver.find_elements(By.CLASS_NAME, value="vehicle-container")
+for a in autos:
+    auto_name = a.find_element(by=By.TAG_NAME, value="img").accessible_name
+#    auto_modelo = driver.find_element(by=By.CLASS_NAME, value='module').text
+    auto_detalle = a.find_element(by=By.CLASS_NAME, value='year').text
+    auto_precio = a.find_element(by=By.CLASS_NAME, value='price-text').text
+    auto_negociable = a.find_element(by=By.CLASS_NAME, value='latam-secondary-text').text
+
+    document = {
+        "modelo_auto": auto_name,
+        "anio_auto": auto_detalle,
+        "precio_auto:": auto_precio,
+        "auto_detalle:": auto_negociable
+    }
+    col.insert_one(document=document)
+
+
+    print(auto_name)
+#    print(auto_modelo)
+    print(auto_detalle)
+    print(auto_precio)
+    print(auto_negociable)
+    print('='*40)
 
    # detalle = a.find_element(by=By.CLASS_NAME, value="vehicle-href")
 # elem.clear()
